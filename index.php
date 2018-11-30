63e0c37c865480ce92cde327036ac97cbb3e100b
ADDCN\10320 <lilinsen@addcn.com>
2018-11-30 16:04:58 +0800

    pk test

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 10320
+ * Date: 2018/11/30
+ * Time: 15:56
+ */
+
+require_once __DIR__. '/vendor/autoload.php';
+
+$user = new \Util\User();
+$user->login();
+
---
+++ src/Util/User.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lingan
+ * Date: 2017/11/6
+ * Time: 21:38
+ */
+
+namespace Util;
+
+class User
+{
+    public function login()
+    {
+        echo "Sam is login";
+    }
+}
